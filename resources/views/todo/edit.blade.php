022f22a7260e05094285e4857c90ecdf2f931749
Yamato Nakagami <yamatonakagami@sss.local>
2017-10-03 19:54:22 +0900

    Initial Commit

---
+++ resources/views/todo/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+
+
+@section('content')
+
+<body>
+<div class="container">
+    <h2 class="page-header">ToDo編集</h2>
+    {!! Form::open(['route' => ['todo.update', $todo->id], 'method' => 'PUT']) !!}
+    <div class="form-group">
+        {!! Form::input('text', 'title', $todo->title, ['required', 'class' => 'form-control']) !!}
+    </div>
+    <button type="submit" class="btn btn-success pull-right">更新</button>
+    {!! Form::close() !!}
+    <!-- Scripts -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</div>
+</body>
+
+@endsection
